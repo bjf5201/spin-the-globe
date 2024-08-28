eac47b0da62af429a993b4863f407a84cd596041
Bethany Fannin <contact@bethanyfannin.com>
2024-08-28 12:45:15 -0400

    style(eslint): fix eslint configuration
    Update the esline.config.json file to utilize the @stylistic configurations for all formatting rules
    per eslint best practices.
    
    Closes #7

--- eslint.config.js
+++ eslint.config.js
@@ -2,36 +2,15 @@
 
 import stylistic from "@stylistic/eslint-plugin";
 
-export default {
-  parserOptions: {
-    ecmaVersion: 2016,
-    sourceType: "module"
-  },
-  plugins: ["@stylistic"],
-  rules: {
-    "@stylistic/indent": ["error", 2],
-    "@stylistic/semi": "error",
-    "@stylistic/quotes": ["error", "double"],
-    "@stylistic/no-trailing-spaces": "error",
-    "@stylistic/exports-last": "error",
-    "@stylistic/exports-order": ["error", { "default": 0, "named": 1 }],
-    "@stylistic/imports-last": "error",
-    "@stylistic/imports-order": ["error", { "builtin": 0, "external": 1, "internal": 2, "parent": 3, "sibling": 4 }],
-    "@stylistic/quote-props": ["error", "as-needed", { "numbers": true, "keywords": true }],
-    "@stylistic/array-bracket-spacing": "always",
-    "@stylistic/arrow-parens": ["error", "as-needed"],
-    "@stylistic/arrow-spacing": "error",
-    "@stylistic/block-spacing": "error",
-    "@stylistic/brace-style": "error",
-    "@stylistic/comma-dangle": ["error", {
-      "arrays": "never",
-      "objects": "always-multiline",
-      "imports": "never",
-      "exports": "never",
-      "functions": "never"
-    }],
-    "@stylistic/comma-spacing": "error",
-    "@stylistic/comma-style": "error",
-    "@stylistic/function-paren-newline": ["error", { "minItems": 3 }],
-  }
-}
+export default [
+  stylistic.configs.customize({
+    // the following options are the default values
+    indent: 2,
+    quotes: "double",
+    semi: true,
+    jsx: true,
+    trailingComma: "none",
+    // ...
+  }),
+  // ...you other config items
+]
